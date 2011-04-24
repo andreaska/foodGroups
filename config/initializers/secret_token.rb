4d28fe84803cd279fbec9fec74dd6d3a42ea915d
= <759487@beuth-hochschule.de>
2011-04-25 00:48:11 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodGroups::Application.config.secret_token = '10a9d66fe094c64982e859369a792e2c5508ceb76d27f71bec896f0ec58817ac0bc5fe1b2a6d6dd770cbdc9285881e9a772ee8517b7c37d561ee8a4fb980682e'
